33c4227533f71a93d73314178dab5998be16f307
Sako Hovaguimian <sakohovaguimian@yahoo.com>
2020-02-09 09:11:00 -0800

    created ext

---
+++ rTale/Extensions/String+ext.swift
@@ -0,0 +1,15 @@
+//
+//  String+ext.swift
+//  rTale
+//
+//  Created by Sako Hovaguimian on 2/9/20.
+//  Copyright © 2020 Sako Hovaguimian. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    static let gameFont = "PressStart2P"
+    
+}
---
+++ rTale/Extensions/UIColor+ext.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+ext.swift
+//  rTale
+//
+//  Created by Sako Hovaguimian on 2/9/20.
+//  Copyright © 2020 Sako Hovaguimian. All rights reserved.
+//
+
+import Animo
+import UIKit
+
+extension UIColor {
+    
+    static let navyGreen = setColor(r: 12.0, g: 12.0, b: 12.0)
+    
+}
