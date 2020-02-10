33f45d508c372dcbfbf8e9488d00234579af98aa
Sako Hovaguimian <sakohovaguimian@yahoo.com>
2020-02-09 16:43:38 -0800

    Added new UI components, and business logic for viewing story details

--- rTale/Coordinators/StoryCoordinator/StoryViewController.swift
+++ rTale/Coordinators/StoryCoordinator/StoryViewController.swift
@@ -17,26 +17,39 @@ class StoryViewController: UIViewController, Storyboarded {
     
     //MARK:- Views
     
-    let myButton: UIButton = {
+    private lazy var storyView: UIView = {
         
-        let button = UIButton(type: .system)
-        button.backgroundColor = .red
-        button.setTitle("Button", for: .normal)
-        button.addTarget(self, action: #selector(storyButtonTapped(sender:)), for: .touchUpInside)
+        let myView = UIView()
+        myView.backgroundColor = .eggShell
+        myView.clipsToBounds = true
+        myView.layer.cornerRadius = 23
         
-        return button
+        myView.addSubview(self.storyLabel)
         
+        self.storyLabel.centerX(inView: myView)
+        self.storyLabel.anchor(top: myView.topAnchor, paddingTop: 20, width: 300, height: 200)
+        
+        
+        return myView
+            
     }()
     
-    let myLabel: UILabel = {
-        
-        let label = UILabel()
-        label.backgroundColor = .eggShell
-        label.textColor = .black
+    private var storyLabel: UITextView = {
+        let label = UITextView()
         label.textAlignment = .center
-        
+        label.backgroundColor = .clear
+        label.textColor = .black
+        label.font = UIFont(name: .gameFont, size: 15.0)
+        label.frame.size = CGSize(width: 100, height: 400)
         return label
-        
+    }()
+
+    private lazy var choicesStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.spacing = 12
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        return stackView
     }()
     
     
@@ -49,37 +62,69 @@ class StoryViewController: UIViewController, Storyboarded {
     
     override func viewDidLoad() {
         self.configureViews()
-        self.view.backgroundColor = .skyBlue
+        self.view.backgroundColor = .black
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    //MARK:- Helper Functions
-    
-    private func configureViews() {
-        
-        self.view.addSubview(self.myButton)
-        
-        self.myButton.centerX(inView: self.view)
-        self.myButton.centerY(inView: self.view)
-        self.myButton.setDimmensions(height: 50, width: 200)
-        
-        self.view.addSubview(self.myLabel)
-        
-        self.myLabel.centerX(inView: self.view)
-        self.myLabel.anchor(bottom: self.myButton.topAnchor, paddingBottom: 45)
-        self.myLabel.setDimmensions(height: 50, width: 200)
-        
-        self.myLabel.text = "\(self.storyViewModel.getStory().title)"
-        
-    }
-    
     //MARK:- Objc Functions
     
     @objc private func storyButtonTapped(sender: UIButton) {
         self.storyViewModel.handleDismissTapped()
+    }
+    
+    @objc private func nextPartOfStoryTapped(sender: UIButton) {
+        
+        //This will probabaly return a scene object and we can dynamically update the choies
+        //Have number of choices accept a scene and loop over in it's own function
+        self.storyViewModel.runloop(label: self.storyLabel)
+        self.numnberOfChoices(choices: [
+            "Turn Left",
+            "Turn Right",
+            "Stay Where You Are"
+        ])
+    }
+    
+    //MARK:- Helper Functions
+    
+    private func configureViews() {
+        
+        self.view.backgroundColor = UIColor(hex: "2A2E32")
+        
+        //Story View
+        self.view.addSubview(self.storyView)
+        self.storyView.anchor(top: self.view.topAnchor ,left: self.view.leftAnchor, right: self.view.rightAnchor, paddingTop: 64, paddingLeft: 32, paddingRight: 32, height: 350)
+        self.storyView.centerX(inView: self.view)
+        
+        //Choices Stack View
+        
+        self.view.addSubview(self.choicesStackView)
+        self.choicesStackView.anchor(top: self.storyView.bottomAnchor,
+                              left: self.view.leftAnchor,
+                              bottom: self.view.bottomAnchor,
+                              right: self.view.rightAnchor,
+                              paddingTop: 200,
+                              paddingLeft: 32,
+                              paddingBottom: 64,
+                              paddingRight: 32)
+        
+    }
+    
+    public func numnberOfChoices(choices: [String]) {
+        
+        self.choicesStackView.arrangedSubviews.forEach({ $0.removeFromSuperview() })
+        
+        for i in choices {
+            
+            let choiceButton = ChoiceButton(text: i)
+            choiceButton.setTitleColor(.black, for: .normal)
+            choiceButton.addTarget(self, action: #selector(self.nextPartOfStoryTapped(sender:)), for: .touchUpInside)
+            self.choicesStackView.addArrangedSubview(choiceButton)
+                        
+        }
+
     }
     
 }
--- rTale/Coordinators/StoryCoordinator/StoryViewModel.swift
+++ rTale/Coordinators/StoryCoordinator/StoryViewModel.swift
@@ -24,6 +24,10 @@ class StoryViewModel {
     
     private var story: Story!
     
+    private var index = 0
+    private var runloop = RunLoop.main
+    private var inGuard = false
+    
     //MARK:- Helper Functions
     
     public func getStory() -> Story {
@@ -34,5 +38,54 @@ class StoryViewModel {
         self.dimissDelegate.dimissStoryVC()
     }
     
+    public func nextPartOfStoryTestData(_ index: Int) -> String {
+        
+        var text = ""
+        
+        switch index {
+            case 0: text = "What’s going on?"
+            case 1: text = "How does everything feel so numb, yet so real."
+            case 2: text = "Where am I? Am I floating?"
+            case 3: text = "What’s this feeling? Someone’s holding me?"
+            case 4: text = "It’s Like a warm hug. Like when I was younger"
+            case 5: text = "This has to be a dream; I know you’re not with us anymore."
+            case 6: text = "I need to find a way out!"
+            default: text = "THE END"
+        }
+        return text
+    }
+    
+     func runloop(label: UITextView) {
+        
+        let string = self.nextPartOfStoryTestData(self.index)
+            
+        guard self.inGuard == false else { return }
+        
+        self.inGuard = true
+        
+        self.runloop.cancelPerformSelectors(withTarget: self.runloop)
+        self.runloop.cancelPerformSelectors(withTarget: self)
+        
+        self.index += 1
+        
+        label.text = ""
+        outerLoop: for i in string {
+
+            label.text! += "\(i)"
+            
+            let style = NSMutableParagraphStyle()
+            style.lineSpacing = 10
+            let attributes = [NSAttributedString.Key.paragraphStyle : style, NSAttributedString.Key.font : UIFont(name: "PressStart2P", size: 15.0)]
+            label.attributedText = NSAttributedString(string: label.text ?? "", attributes: attributes as [NSAttributedString.Key : Any])
+            
+            if String(i) != "" {
+                self.runloop.run(until: Date() + 0.050)
+            }
+            
+        }
+        
+        self.inGuard = false
+        
+    }
 
 }
